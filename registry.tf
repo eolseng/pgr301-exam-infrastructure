b656213fe4b22f1476e48c11b3b7cfb10d46d0dd
Erik Olseng <e.olseng@gmail.com>
2020-11-18 16:55:22 +0100

    Add Container Registry Location

--- registry.tf
+++ registry.tf
@@ -1,4 +1,4 @@
 resource "google_container_registry" "registry" {
   project = var.project_name
-  location = var.project_region
+  location = var.container_registry_location
 }
--- vars.tf
+++ vars.tf
@@ -1,15 +1,26 @@
 variable "project_name" {
-  description = "The name of the GCP project"
+  description = "The name of the GCP project."
   type = string
 }
 
 variable "project_region" {
-  description = "The region of the GCP project"
+  description = "The region of the GCP project."
   type = string
 }
 
 variable "project_keyfile" {
-  description = "Path to the keyfile of the GCP Service Account to run Terraform"
+  description = "Path to the keyfile of the GCP Service Account to run Terraform."
   type = string
   default = "gcp_keyfile.json"
+}
+
+variable "container_registry_location" {
+  description = "The location of the Container Registry."
+  type = string
+  default = "EU"
+
+  validation {
+    condition = contains(["EU", "US", "ASIA"], var.container_registry_location)
+    error_message = "Container Registry Location must be one of the following: ['EU', 'US', 'ASIA']."
+  }
 }
