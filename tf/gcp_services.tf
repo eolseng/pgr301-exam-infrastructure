56e39039fab23687aef9e6ea50f7ac55b129260b
Erik Olseng <e.olseng@gmail.com>
2020-11-18 23:36:42 +0100

    Enable Cloud Run service

--- tf/gcp_services.tf
+++ tf/gcp_services.tf
@@ -2,3 +2,8 @@ resource "google_project_service" "container_registry" {
   project = var.project_name
   service = "containerregistry.googleapis.com"
 }
+
+resource "google_project_service" "cloud_run" {
+  project = var.project_name
+  service = "run.googleapis.com"
+}
