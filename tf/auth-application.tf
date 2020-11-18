f4f530c2440d13c7b87f417825917ff477cb6d51
Erik Olseng <e.olseng@gmail.com>
2020-11-18 23:45:41 +0100

    Add Cloud Run instance with Auth application

---
+++ tf/auth-application.tf
@@ -0,0 +1,27 @@
+resource "google_cloud_run_service" "auth_application" {
+
+  name = "auth_application"
+  location = var.project_region
+
+  metadata {
+    annotations = {
+      "run.googleapis.com/client-name" = "terraform"
+    }
+  }
+
+  template {
+    spec {
+      containers {
+        image = "eu.gcr.io/${var.project_name}/pgr301-exam-auth:latest"
+      }
+    }
+  }
+
+  traffic {
+    latest_revision = true
+    percent = 100
+  }
+
+  autogenerate_revision_name = true
+
+}
