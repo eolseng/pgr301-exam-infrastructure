54986e09decc59ef9512f794b4ec7fa701b7e76b
Erik Olseng <e.olseng@gmail.com>
2020-11-22 21:53:36 +0100

    Update statuscake test and app image

--- tf/auth-application.tf
+++ tf/auth-application.tf
@@ -10,7 +10,7 @@ resource "google_cloud_run_service" "auth_application" {
   template {
     spec {
       containers {
-        image = "eu.gcr.io/${var.project_name}/pgr301-exam-auth:1f9bc80cb568b471e1bce806af60394db6cab5d4"
+        image = "eu.gcr.io/${var.project_name}/pgr301-exam-auth:12c23534a02478580182f3de63ac17ae649b362a"
         # Database
         env {
           name = "AUTH_DB_URL"
--- tf/statuscake.tf
+++ tf/statuscake.tf
@@ -1,6 +1,6 @@
-resource "statuscake_test" "auth_app_actuator" {
+resource "statuscake_test" "auth_app" {
   website_name  = google_cloud_run_service.auth_application.name
-  website_url   = "${google_cloud_run_service.auth_application.status[0].url}/actuator/health"
+  website_url   = google_cloud_run_service.auth_application.status[0].url
   test_type     = "HTTP"
   check_rate    = 300
   contact_group = [var.statuscake_contact_group]
