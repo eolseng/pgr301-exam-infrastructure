5a310a8f334e4b1579301a0a3769df6b5adb9a63
Erik Olseng <e.olseng@gmail.com>
2020-11-22 17:17:08 +0100

    Add PostgreSQL database for AuthApp

--- tf/auth-application.tf
+++ tf/auth-application.tf
@@ -7,13 +7,18 @@ resource "google_cloud_run_service" "auth_application" {
     spec {
       containers {
         image = "eu.gcr.io/${var.project_name}/pgr301-exam-auth:897180d59c8e2b28800ac94fd72b9e7a2339e553"
-        env{
+        env {
           name = "LOGZIO_TOKEN"
           value = var.logzio_token
         }
         env {
           name = "LOGZIO_URL"
           value = var.logzio_url
+        }
+        env {
+          # Set the Spring Profile to 'prod' to enable LOGZ.IO and disable InfluxDB exporting
+          name = "SPRING_PROFILES_ACTIVE"
+          value = "prod"
         }
         resources {
           limits = {
@@ -27,6 +32,7 @@ resource "google_cloud_run_service" "auth_application" {
   metadata {
     annotations = {
       "run.googleapis.com/client-name" = "terraform"
+      "run.googleapis.com/cloudsql-instances" = "${var.project_name}:${var.project_region}:${google_sql_database_instance.auth-db.name}"
     }
   }
   traffic {
@@ -51,4 +57,13 @@ resource "google_cloud_run_service_iam_policy" "auth_application_noauth" {
   service = google_cloud_run_service.auth_application.name
 
   policy_data = data.google_iam_policy.cloud_run_noauth.policy_data
+}
+
+resource "google_sql_database_instance" "auth-db" {
+  name = "postgres-instance-auth-db"
+  region = var.project_region
+  database_version = "POSTGRES_12"
+  settings {
+    tier = "db-f1-micro"
+  }
 }
